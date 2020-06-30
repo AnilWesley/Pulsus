2b41d06467829b5a383b619fbe8d4e980e78e6c4
Home <anilwesley94@gmail.com>
2020-06-30 19:48:51 +0530

    anil commit

--- app/src/main/java/com/applications/pulsus/adapters/BannerAdapter.java
+++ app/src/main/java/com/applications/pulsus/adapters/BannerAdapter.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.smarteist.autoimageslider.SliderViewAdapter;
 
+
 import java.util.List;
 
 public class BannerAdapter extends SliderViewAdapter<BannerAdapter.SliderAdapterVH> {
